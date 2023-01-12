726511f5c910edbf10a6d8de8d650008d29420a5
ToshaBY <anton.rybakou@gmail.com>
2023-01-12 16:54:41 +0300

    feat: eslint prettier config

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,6 @@
-import React, { ReactNode } from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import React, { ReactNode } from 'react';
+import Link from 'next/link';
+import Head from 'next/head';
 
 type Props = {
   children?: ReactNode
@@ -11,14 +11,14 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
   <div>
     <Head>
       <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta charSet='utf-8' />
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
     </Head>
     <header>
       <nav>
-        <Link href="/">Home</Link> | <Link href="/about">About</Link> |{' '}
-        <Link href="/users">Users List</Link> |{' '}
-        <Link href="/api/users">Users API</Link>
+        <Link href='/'>Home</Link> | <Link href='/about'>About</Link> |{' '}
+        <Link href='/users'>Users List</Link> |{' '}
+        <Link href='/api/users'>Users API</Link>
       </nav>
     </header>
     {children}
@@ -27,6 +27,6 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <span>I'm here to stay (Footer)</span>
     </footer>
   </div>
-)
+);
 
-export default Layout
+export default Layout;
