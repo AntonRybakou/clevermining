45a1ffa5fc3b18ba5f21386ca5e529e031d803d9
ToshaBY <anton.rybakou@gmail.com>
2023-01-18 23:48:57 +0300

    refactor: css

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,29 +5,11 @@ import Layout from '../components/Layout';
 const IndexPage = (): ReactElement => (
   <Layout title="Clevermining">
     <h1>MAIN PAGE</h1>
-    <div>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aperiam
-      architecto consequuntur ducimus error, ex expedita ipsam optio saepe suscipit! A
-      accusamus alias asperiores at blanditiis dicta dolores doloribus dolorum ea enim,
-      excepturi fugit illo inventore iste iusto modi necessitatibus nulla, pariatur
-      perspiciatis quae quasi qui quisquam repellat similique sint tempora totam,
-      voluptatibus. Ab accusamus aperiam atque corporis debitis eos eveniet id itaque
-      libero officia porro quam quos repellat soluta suscipit, tempora voluptatum. At
-      cumque cupiditate, debitis delectus deserunt dolorum ea eius excepturi
-      exercitationem facere ipsa iure magni modi quisquam quod ratione sed veniam. Iste
-      necessitatibus, perspiciatis. Eligendi, saepe, temporibus.
-    </div>
+    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
     <hr />
     <div>
       Принимая во внимание, что признание достоинства, присущего всем членам человеческой
-      семьи, и равных и неотъемлемых прав их является основой свободы, справедливости и
-      всеобщего мира; и принимая во внимание, что пренебрежение и презрение к правам
-      человека привели к варварским актам, которые возмущают совесть человечества, и что
-      создание такого мира, в котором люди будут иметь свободу слова и убеждений и будут
-      свободны от страха и нужды, провозглашено как высокое стремление людей; и принимая
-      во внимание, что необходимо, чтобы права человека охранялись властью закона в целях
-      обеспечения того, чтобы человек не был вынужден прибегать, в качестве последнего
-      средства, к восстанию против тирании и угнетения; и
+      семьи
     </div>
   </Layout>
 );
