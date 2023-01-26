da5bc7e05426e0dcd219e53309f2e0310fc0bd11
ToshaBY <anton.rybakou@gmail.com>
2023-01-26 23:23:40 +0300

    refactor: main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,58 +6,6 @@ const IndexPage = (): ReactElement => (
   <Layout title="Clevermining">
     <h1>MAIN PAGE</h1>
     <hr />
-    <div className="text-xl">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut blanditiis dicta
-      dolorem ipsum mollitia vero! Ab corporis distinctio excepturi facilis fugiat, iure
-      maxime perspiciatis quibusdam quidem vel. Ab, blanditiis culpa cum delectus deserunt
-      dicta dignissimos dolor eos est exercitationem hic magnam maxime nesciunt nisi non
-      officia omnis quaerat quia quisquam recusandae suscipit tempora ut. Deleniti
-      doloribus magnam molestias nostrum quae qui quidem reiciendis sed unde vitae.
-      Consequuntur deserunt est fugiat impedit sunt tempora vel voluptate. A cupiditate
-      dolores id nemo perspiciatis provident totam. Aliquid aut cumque deserunt dolor,
-      excepturi hic modi quia. Aliquam aspernatur atque dolores eius numquam sed tempora?
-      Accusamus ad adipisci animi aperiam dolor ducimus laudantium, modi neque recusandae.
-      Commodi eius magnam nobis perspiciatis, quos reprehenderit sed voluptatum! A ab
-      dolores magni modi praesentium. Incidunt magni mollitia quae ratione ut? Adipisci
-      architecto blanditiis, eaque iste nemo velit. Doloribus explicabo fuga iusto laborum
-      maxime, molestias non pariatur possimus quia ratione. Beatae error fugit laboriosam,
-      numquam quaerat repellat rerum temporibus? Accusantium aliquid architecto asperiores
-      blanditiis deserunt dicta dolorem eveniet facilis impedit, in inventore ipsa itaque
-      iusto natus odit officia placeat porro praesentium quisquam rem repellat saepe unde
-      velit veniam voluptas? Autem cum esse est expedita facere id incidunt ipsa,
-      laudantium molestiae mollitia neque nesciunt officia quasi repellat sit vero, vitae.
-      Aliquid consectetur enim eos eum fuga fugiat illo ipsum iste, maxime nisi nobis
-      nulla omnis optio provident quas reiciendis reprehenderit saepe sapiente soluta
-      sunt. Aperiam asperiores ipsa minima omnis ullam. Doloremque nostrum provident
-      tempora. Ad aliquid at beatae blanditiis consectetur culpa cum dolore doloribus,
-      eaque est facilis fugiat hic iste iusto magni minima molestias nihil nobis
-      praesentium quod repellat sapiente temporibus totam ullam voluptatibus? Assumenda
-      cumque cupiditate dignissimos dolor ducimus, eaque, excepturi itaque magni molestias
-      necessitatibus quasi ratione, sequi voluptatum! Aperiam atque aut corporis dicta eum
-      iste laboriosam magnam maiores, minima modi molestiae pariatur sequi vel voluptate
-      voluptates. Ad architecto consectetur cum dolor eveniet ex facilis fugiat illo
-      libero, magnam minus modi molestias nisi nulla officiis optio quasi quod repudiandae
-      sapiente similique tempore totam velit voluptas voluptates voluptatibus? Amet
-      architecto asperiores, assumenda at atque consequatur consequuntur cupiditate
-      debitis deleniti dicta doloremque dolores doloribus minima minus nemo nulla
-      praesentium quibusdam quod sequi similique sunt vel velit. Adipisci ipsa officiis
-      porro possimus praesentium recusandae sunt tempora. At, maiores non nostrum odio
-      porro possimus quibusdam rem. Accusantium atque debitis dicta dolor error esse est,
-      eveniet expedita iste iure magni maiores maxime nam numquam odit placeat quae
-      quaerat quam, quas, quibusdam quod recusandae rerum tempora! Alias cum dicta dolorum
-      expedita explicabo, laudantium optio pariatur ullam voluptatibus. Ab accusamus
-      aliquam aut autem consequatur consequuntur doloremque, ducimus expedita labore magni
-      nemo omnis quis quos, rem repellat sint suscipit. Accusantium alias asperiores autem
-      consequatur deleniti dolores error esse harum illo in ipsa, iste iusto molestias
-      mollitia nisi nulla numquam perspiciatis quasi qui reiciendis saepe sit totam!
-      Accusamus at, cum distinctio excepturi facilis impedit in ipsum nam necessitatibus
-      qui quisquam ratione soluta vel? Asperiores aspernatur dolorem fugit nobis, possimus
-      praesentium, provident quidem recusandae repellat sint soluta velit vitae.
-    </div>
-
-    <hr />
-
-    <hr />
     <div>
       Принимая во внимание, что признание достоинства, присущего всем членам человеческой
       семьи
